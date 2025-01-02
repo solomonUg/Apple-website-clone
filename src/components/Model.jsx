8f7ace17ecf18c2a46ad9c217ddc1c19da020ee8
SolomonUg <solomonchris700@gmail.com>
2025-01-02 19:27:14 +0100

    added another section

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -6,6 +6,7 @@ import {yellowImg} from "../utils";
 import * as THREE from 'three';
 import { Canvas } from '@react-three/fiber';
 import { View } from '@react-three/drei';
+import { models, sizes } from "../constants";
 
 
 
@@ -68,10 +69,20 @@ const [largeRotation, setLargeRotation] = useState(0);
                 </div>
                 <div className='mx-auto w-full'>
                     <p className='text-sm font-light text-center mb-5'>{model.title}</p>
-                    <div className="flex-center "></div>
+                    <div className="flex-center ">
                     <ul className='color-container'>
-
+                        {models.map((item, i)=><li key={i} className='w-6 h-6 rounded-full mx-2 cursor-pointer' style={{backgroundColor:item.color[0]}} 
+                        onClick={()=> setModel(item)}
+                        ></li>)}
                     </ul>
+                    <button className="size-btn-container">
+                {sizes.map(({ label, value }) => (
+                  <span key={label} className="size-btn" style={{ backgroundColor: size === value ? 'white' : 'transparent', color: size === value ? 'black' : 'white'}} onClick={() => setSize(value)}>
+                    {label}
+                  </span>
+                ))}
+              </button>
+                    </div>
                 </div>
             </div>
         </div>
